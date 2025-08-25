fe1155fdb49db4dbdca8c43307161d63cb7aeb91
Arthurchico <arthurchico10@gmail.com>
2025-08-25 11:54:15 +0000

    mediaaula

---
+++ Estudos/media.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Fazer um programa que le 30 notas para imprimir a media 
+//e imprimir todos os numeros acima da media 
+
+int main(){
+
+int soma = 0;
+double media;
+int numeros [TAM];
+FOR (int i =0; i < TAM; i +- 1){
+
+    scanf("%d", &numeros[i]);
+
+    soma += numeros[i]
+}
+
+media = soma/30.0;
+    return 0;
+}
