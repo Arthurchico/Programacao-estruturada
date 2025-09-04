ab1ba36ce622fa646c079ca70ff347f1f6a8c611
Arthurchico <arthurchico10@gmail.com>
2025-09-04 11:37:45 +0000

    vetoratividade1

---
+++ Estudos/imcsozinho.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+double idade , peso, altura, imc;
+
+printf("Digite sua idade: ");
+scanf("%lf" , &idade);
+
+printf("Digite sua altura: ");
+scanf("lf ", &altura);
+
+printf("Digite seu peso:");
+scanf("lf" , &peso);
+
+return 0;
+
+}
+
