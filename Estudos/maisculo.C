5c7d52f0866db6b8eeec7cae7db4cf1efba38dc1
Arthurchico <arthurchico10@gmail.com>
2025-10-09 11:19:25 +0000

    atvhoje

---
+++ Estudos/maisculo.C
@@ -0,0 +1,3 @@
+/**como faço para resolver essa questão sem implementar nada 
+ * fazendo tudo na mão em linguagem C 
+ * transforme todas essas letras em maisculo char frase [50]"Ola, Turma!"
--- Estudos/string.C
+++ Estudos/string.C
@@ -1,1 +1,9 @@
-aaaaaaaaaaaaaaaa
+#include <stdio.h>
+
+int main(){
+
+    char nome [50] = "Maria";
+    nome [5] = '-';
+    printf("nome = '%s'", nome);
+    return 0;
+}
