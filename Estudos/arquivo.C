90e59526e929f5fb11140fca1de048e739a6d75b
Arthurchico <arthurchico10@gmail.com>
2025-10-27 11:55:18 +0000

    arquivo

---
+++ Estudos/arquivo.C
@@ -0,0 +1,17 @@
+// Criar o registro Aluno que tem os campos nota1 e nota2
+// Preencher e imprimir as informações de aluno, incluindo a sua media (que deve ser calculada
+// Usando peso 2 para a nota1 e peso 3 para nota2)
+
+#include <stdio.h>
+
+int main(){
+
+    
+
+
+
+
+}
+
+
+
